8b1072febc9be45eb6b2e30c49c1032d30173b3d
kelvin8wong <kelvin8wong@gmail.com>
2017-10-05 19:51:18 +0000

    refactory found waldo

--- callback.js
+++ callback.js
@@ -1,11 +1,11 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+arr.forEach(findWaldo(arr, found) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
     }
-  }
-}
+});
+
+
 
 function actionWhenFound(i) {
   console.log("Found Waldo at index " + i);
